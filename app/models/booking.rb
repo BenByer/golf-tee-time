ce675f6ddba37b66ea555d08ec004d5d865bbdc2
goodbru <ryangoodhew@gmail.com>
2016-04-21 23:13:53 +0000

    planted seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,4 @@ class Booking < ActiveRecord::Base
 
     has_many :users
 
-    def bookings_by_date(requested_date)
-      Booking.where(SELECT * FROM :bookings WHERE :tee_time_at BETWEEN CONVERT(datetime, 'requested_date'))
-    end
-
 end
