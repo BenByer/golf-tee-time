33381db2f6c4887a5db2f4c3efb8ee2c60f11783
BenByer <benbyer@shaw.ca>
2016-04-21 16:23:36 -0700

    working on time-chooser

---
+++ app/models/booking.rb
@@ -0,0 +1,9 @@
+class Booking < ActiveRecord::Base
+
+    has_many :users
+
+    # def bookings_by_date(requested_date)
+    #   Booking.where(SELECT * FROM :bookings WHERE :tee_time_at BETWEEN CONVERT(datetime, 'requested_date'))
+    # end
+
+end
