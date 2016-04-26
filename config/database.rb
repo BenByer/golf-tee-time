2f794f60862d6308f45d225c450844f4eabebd20
Kiefer <kiefer136@hotmail.com>
2016-04-26 13:01:29 -0700

    setting up for heroku

--- config/database.rb
+++ config/database.rb
@@ -1,25 +1,11 @@
 configure do
   # Log queries to STDOUT in development
-  if Sinatra::Application.development?
-    ActiveRecord::Base.logger = Logger.new(STDOUT) 
-      set :database, {
-        adapter: "sqlite3",
-        database: "db/db.sqlite3"
-      }
-
-  else 
+  if development?
     set :database, {
-  adapter: "postgresql",
-  database: "tee_it_up_database_production"
-      } 
+      adapter: "sqlite3"
+      database: "db/db.sqlite3"
+    }
+  else
+    set :database, ENV['DATABASE_URl']
   end
-
-
-  # Load all models from app/models, using autoload instead of require
-  # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
-  Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
-    filename = File.basename(model_file).gsub('.rb', '')
-    autoload ActiveSupport::Inflector.camelize(filename), model_file
-  end
-
 end
